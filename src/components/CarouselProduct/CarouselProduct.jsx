6d632cb5d05b7cc96c23e99e14d387e0a6e07398
syahrulmf <syahrulmf@gmail.com>
2022-06-14 23:38:57 +0700

    Add Detail Product : Header, Carousel, Content

---
+++ src/components/CarouselProduct/CarouselProduct.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Product from "../../assets/image/products/product-1.png";
+import btn_right from "../../assets/icons/btn-right.png";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+const CarouselProduct = () => {
+  const slideImages = [Product, Product];
+
+  const slideProperty = {
+    indicators: true,
+    infinite: false,
+    autoplay: false,
+    prevArrow: null,
+    // nextArrow: (
+    //   <div className="absolute cursor-pointer">
+    //     <img src={btn_right} alt="btn" />
+    //   </div>
+    // ),
+  };
+
+  return (
+    <>
+      <Slide {...slideProperty} className="p-0">
+        {slideImages.map((item, index) => {
+          return (
+            <div
+              className="mb-6 flex w-full justify-center overflow-hidden rounded-2xl"
+              key={index}
+            >
+              <img src={item} alt="Product" />
+            </div>
+          );
+        })}
+      </Slide>
+    </>
+  );
+};
+
+export default CarouselProduct;

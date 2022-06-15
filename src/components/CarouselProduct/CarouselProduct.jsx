4b51422cd7831eb727a4a6119ff57797a8036471
syahrulmf <syahrulmf@gmail.com>
2022-06-15 23:45:22 +0700

    remove react-slideshow-image

--- src/components/CarouselProduct/CarouselProduct.jsx
+++ src/components/CarouselProduct/CarouselProduct.jsx
@@ -1,42 +1,14 @@
 import React from "react";
 import Product from "../../assets/image/products/product-1.png";
-// import btn_right from "../../assets/icons/btn-right.png";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
 
 const CarouselProduct = () => {
-  const slideImages = [Product, Product];
-
-  const slideProperty = {
-    indicators: true,
-    infinite: false,
-    autoplay: false,
-    // prevArrow: (
-    //   <span className="cursor-pointer">
-    //     <img src={btn_right} alt="btn" />
-    //   </span>
-    // ),
-    // nextArrow: (
-    //   <span className="cursor-pointer">
-    //     <img src={btn_right} alt="btn" />
-    //   </span>
-    // ),
-  };
+  // const slideImages = [Product, Product];
 
   return (
     <>
-      <Slide {...slideProperty} className="mb-4">
-        {slideImages.map((img, index) => {
-          return (
-            <div
-              className="w-full justify-center overflow-hidden rounded-2xl"
-              key={index}
-            >
-              <img src={img} alt="Product" />
-            </div>
-          );
-        })}
-      </Slide>
+      <div className="mb-6 w-full justify-center overflow-hidden rounded-2xl">
+        <img src={Product} alt="Product" />
+      </div>
     </>
   );
 };

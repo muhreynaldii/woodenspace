738d920b1195efd2587b74184be9843cc6dfd664
syahrulmf <syahrulmf@gmail.com>
2022-06-15 23:45:07 +0700

    Update List Product

--- src/pages/ListProduct/ListProduct.jsx
+++ src/pages/ListProduct/ListProduct.jsx
@@ -3,6 +3,11 @@ import Header from "../../components/Header/Header";
 import User from "../../assets/image/user.png";
 import Product from "../../assets/image/products/product-1.png";
 import fi_plus from "../../assets/icons/fi_plus.svg";
+import fi_box from "../../assets/icons/fi_box.svg";
+import fi_chevron from "../../assets/icons/fi_chevron-right.svg";
+import fi_chevron_1 from "../../assets/icons/fi_chevron-right-1.svg";
+import fi_heart from "../../assets/icons/fi_heart.svg";
+import fi_dollar from "../../assets/icons/fi_dollar-sign.svg";
 import { Link } from "react-router-dom";
 
 const ListProduct = () => {
@@ -28,7 +33,7 @@ const ListProduct = () => {
                 </div>
 
                 <div>
-                  <button className="rounded-lg border border-dark-blue-04 py-1 px-3 text-xs font-medium text-neutral-05">
+                  <button className="rounded-lg border border-purple-04 py-1 px-3 text-xs font-medium text-neutral-05 transition duration-300 hover:bg-purple-04 hover:text-white">
                     Edit
                   </button>
                 </div>
@@ -39,29 +44,59 @@ const ListProduct = () => {
           {/* list product box */}
           <div className="flex justify-center">
             <div className="w-full lg:w-[968px]">
-              <div className="flex flex-col justify-between lg:flex-row">
+              <div className="flex flex-col lg:flex-row">
                 <div className="mr-8 mb-6 h-[248px] w-full rounded-2xl p-6 shadow-low md:w-full lg:w-[268px]">
                   <h3 className="mb-6 text-base font-semibold">Kategori</h3>
                   <ul>
                     <li className="border-b-neutral-01 border-b-2 pb-4">
-                      Semua Produk
+                      <div className="flex items-center">
+                        <img src={fi_box} alt="" className="mr-2" />
+                        <Link
+                          to="#"
+                          className="mr-7 text-base font-medium text-purple-04"
+                        >
+                          Semua Produk
+                        </Link>
+                        <img src={fi_chevron} alt="" />
+                      </div>
                     </li>
                     <li className="border-b-neutral-01 mt-4 border-b-2 pb-4">
-                      Diminati
+                      <div className="flex items-center">
+                        <img src={fi_heart} alt="" className="mr-2" />
+                        <Link
+                          to="#"
+                          className="mr-[78px] text-base font-medium text-black"
+                        >
+                          Diminati
+                        </Link>
+                        <img src={fi_chevron_1} alt="" />
+                      </div>
                     </li>
-                    <li className="mt-4">Terjual</li>
+                    <li className="mt-4">
+                      <div className="flex items-center">
+                        <img src={fi_dollar} alt="" className="mr-2" />
+                        <Link
+                          to="#"
+                          className="mr-[90px] text-base font-medium text-black"
+                        >
+                          Terjual
+                        </Link>
+                        <img src={fi_chevron_1} alt="" />
+                      </div>
+                    </li>
                   </ul>
                 </div>
                 <div className="flex w-full flex-wrap justify-between lg:w-3/4">
-                  <Link to="/detail">
-                    <div className="border-neutral-01 mb-6 flex h-[198px] w-full flex-col items-center justify-center rounded-md border-2 border-dashed p-6 lg:w-[207px]">
-                      <img src={fi_plus} alt="icon_add" />
-                      <p className="mt-3 text-sm font-normal text-neutral-03">
-                        Tambah Produk
-                      </p>
-                    </div>
-                  </Link>
-                  <div className="mb-6 h-[198px] w-full rounded-md p-2 shadow-low lg:w-[207px]">
+                  {/* <Link to="/detail"> */}
+                  <div className="border-neutral-01 mb-6 flex h-[198px] w-full flex-col items-center justify-center rounded-md border-2 border-dashed p-6 hover:cursor-pointer lg:w-[207px]">
+                    <img src={fi_plus} alt="icon_add" />
+                    <p className="mt-3 text-sm font-normal text-neutral-03">
+                      Tambah Produk
+                    </p>
+                  </div>
+                  {/* </Link> */}
+
+                  <div className="mb-6 h-[198px] w-[48%] rounded-md p-2 shadow-low lg:w-[207px]">
                     <div
                       className="mb-2 h-24 overflow-hidden rounded-md bg-cover bg-center p-2"
                       style={{ backgroundImage: `url(${Product})` }}
@@ -70,7 +105,7 @@ const ListProduct = () => {
                     <p className="mb-2 text-xs text-neutral-03">Aksesoris</p>
                     <p className="mb-3 text-sm font-normal">Rp. 250.000</p>
                   </div>
-                  <div className="mb-6 h-[198px] w-full rounded-md p-2 shadow-low lg:w-[207px]">
+                  <div className="mb-6 h-[198px] w-[48%] rounded-md p-2 shadow-low lg:w-[207px]">
                     <div
                       className="mb-2 h-24 overflow-hidden rounded-md bg-cover bg-center p-2"
                       style={{ backgroundImage: `url(${Product})` }}
@@ -79,7 +114,7 @@ const ListProduct = () => {
                     <p className="mb-2 text-xs text-neutral-03">Aksesoris</p>
                     <p className="mb-3 text-sm font-normal">Rp. 250.000</p>
                   </div>
-                  <div className="mb-6 h-[198px] w-full rounded-md p-2 shadow-low lg:w-[207px]">
+                  <div className="mb-6 h-[198px] w-[48%] rounded-md p-2 shadow-low lg:w-[207px]">
                     <div
                       className="mb-2 h-24 overflow-hidden rounded-md bg-cover bg-center p-2"
                       style={{ backgroundImage: `url(${Product})` }}
@@ -88,7 +123,7 @@ const ListProduct = () => {
                     <p className="mb-2 text-xs text-neutral-03">Aksesoris</p>
                     <p className="mb-3 text-sm font-normal">Rp. 250.000</p>
                   </div>
-                  <div className="mb-6 h-[198px] w-full rounded-md p-2 shadow-low lg:w-[207px]">
+                  <div className="mb-6 h-[198px] w-[48%] rounded-md p-2 shadow-low lg:w-[207px]">
                     <div
                       className="mb-2 h-24 overflow-hidden rounded-md bg-cover bg-center p-2"
                       style={{ backgroundImage: `url(${Product})` }}
@@ -97,7 +132,7 @@ const ListProduct = () => {
                     <p className="mb-2 text-xs text-neutral-03">Aksesoris</p>
                     <p className="mb-3 text-sm font-normal">Rp. 250.000</p>
                   </div>
-                  <div className="mb-6 h-[198px] w-full rounded-md p-2 shadow-low lg:w-[207px]">
+                  <div className="mb-6 h-[198px] w-[48%] rounded-md p-2 shadow-low lg:w-[207px]">
                     <div
                       className="mb-2 h-24 overflow-hidden rounded-md bg-cover bg-center p-2"
                       style={{ backgroundImage: `url(${Product})` }}

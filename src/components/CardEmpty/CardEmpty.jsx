444e26fe9377d98a19eb7b73f0332b16637e8ac5
syahrulmf <syahrulmf@gmail.com>
2022-06-21 20:16:49 +0700

    add wishlist page

---
+++ src/components/CardEmpty/CardEmpty.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Empty from "../../assets/image/empty.png";
+
+const CardEmpty = () => {
+  return (
+    <>
+      <div className="w-[268px]">
+        <img src={Empty} alt="Empty" />
+        <p className="mt-6 text-sm font-normal text-neutral-05">
+          Belum ada produkmu yang diminati nih, sabar ya rejeki nggak kemana kok
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default CardEmpty;
---
+++ src/pages/Wishlist/Wishlist.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import User from "../../assets/image/user.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import NavMenu from "../../components/NavMenu/NavMenu";
+import CardCategory from "../../components/CardCategory/CardCategory";
+import CardEmpty from "../../components/CardEmpty/CardEmpty";
+
+const Wishlist = () => {
+  return (
+    <>
+      <Header>
+        <NavMenu />
+      </Header>
+
+      <section id="list-product" className="pt-32">
+        <div className="container mx-auto px-4">
+          {/* Box */}
+          <div className="mb-6 flex justify-center">
+            <div className="w-full lg:w-[968px]">
+              <h3 className="mb-6 text-xl font-bold">Daftar Jualan Saya</h3>
+              <div className="flex w-full items-center justify-between rounded-2xl p-4 shadow-low">
+                <div className="flex items-center">
+                  <div className="overflow-hidden rounded-2xl">
+                    <img src={User} alt="User" />
+                  </div>
+                  <div className="ml-4">
+                    <p className="text-sm font-medium">Nama Penjual</p>
+                    <p className="text-xs text-neutral-03">Kota</p>
+                  </div>
+                </div>
+
+                <div>
+                  <button className="rounded-lg border border-purple-04 py-1 px-3 text-xs font-medium text-neutral-05 transition duration-300 hover:bg-purple-04 hover:text-white">
+                    Edit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* list product box */}
+          <div className="flex justify-center">
+            <div className="w-full lg:w-[968px]">
+              <div className="flex flex-col lg:flex-row">
+                {/* Card Kategori */}
+                <CardCategory />
+                {/* Card Kategori */}
+                <div className="mx-auto flex w-full flex-col items-center justify-center text-center lg:w-3/4">
+                  {/* Empty Card */}
+                  <CardEmpty />
+                  {/* Empty Card */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Wishlist;
